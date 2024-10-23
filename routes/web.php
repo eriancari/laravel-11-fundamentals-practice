775ddd0eeaeccd88e8bc795d9886a4ad8ed9ac8f
keicarriedo <erika.liongco@gmail.com>
2024-10-23 16:32:35 +0800

    naming routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "About page";
+});
+
+Route::get('/contact', function () {
+    return "Contact page";
+});
+
+// passing with parameters
+Route::get('/post/{id}', function ($id) {
+    return "Post ID is " . $id;
+});
+
+Route::get('/post/{id}/{name}', function ($id, $name) {
+    return "Post ID is " . $id . " by " . $name;
+});
+
+// naming routes
+Route::get('/admin/post/example', array('as' => 'admin.home', function () {
+    
+    $url = route('admin.home');
+
+    // this can be used like: <a href="route(admin.home)">Click here</a>
+    return "This url is " . $url;
+}));
+
+Route::group(['middleware' =>['web']], function() {
+
+
+});
