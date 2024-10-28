1cf25ea51b7440b57ff059e5ce359d936ab9aa12
keicarriedo <erika.liongco@gmail.com>
2024-10-28 19:40:55 +0800

    raw SQL execution using routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -45,4 +46,30 @@ Route::get('/summary/{id}/{name}/{password}', [PostsController::class, 'summary'
 Route::group(['middleware' =>['web']], function() {
 
 
+});
+
+/* Application Routes */
+
+Route::get('/insert', function() {
+    DB::insert('insert into posts(title, content) values(?, ?)', ['PHP with Laravel', 'Laravel is the best thing that happened to PHP']);
+});
+
+Route::get('/read', function() {
+    $results = DB::select('SELECT * FROM posts where id = ?', [1]);
+
+    return $results;
+    /* foreach($results as $post) {
+        return $post->content;
+    } */
+});
+
+Route::get('/update', function() {
+    $updated = DB::update('UPDATE posts SET title = "Updated title" WHERE id = ?', [1]);
+
+    return $updated;
+});
+
+Route::get('/delete', function() {
+    $deleted = DB::delete('DELETE FROM posts WHERE id = ?', [1]);
+    return $deleted;
 });
