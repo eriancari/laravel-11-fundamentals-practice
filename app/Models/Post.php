e7e7e911f176ea5ca24c6815a748f03fb82e8f8e
keicarriedo <erika.liongco@gmail.com>
2024-11-04 19:19:44 +0800

    CRUD; soft and force delete implementation

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    
+    // $fillable - to allow mass assignment on a create method
+    protected $fillable = [
+        'title',
+        'content'
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use App\Http\Controllers\PostsController;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -50,26 +52,125 @@ Route::group(['middleware' =>['web']], function() {
 
 /* Application Routes */
 
-Route::get('/insert', function() {
-    DB::insert('insert into posts(title, content) values(?, ?)', ['PHP with Laravel', 'Laravel is the best thing that happened to PHP']);
-});
+// Route::get('/insert', function() {
+//     DB::insert('insert into posts(title, content) values(?, ?)', ['Practice Laravel', 'Laravel is the best thing that happened to PHP']);
+// });
 
+// Route::get('/read', function() {
+//     $results = DB::select('SELECT * FROM posts where id = ?', [1]);
+
+//     return $results;
+//     /* foreach($results as $post) {
+//         return $post->content;
+//     } */
+// });
+
+// Route::get('/update', function() {
+//     $updated = DB::update('UPDATE posts SET title = "Updated title" WHERE id = ?', [1]);
+
+//     return $updated;
+// });
+
+// Route::get('/delete', function() {
+//     $deleted = DB::delete('DELETE FROM posts WHERE id = ?', [1]);
+//     return $deleted;
+// });
+
+/** 
+ * ELOQUENT
+*/
+
+// READ
 Route::get('/read', function() {
-    $results = DB::select('SELECT * FROM posts where id = ?', [1]);
-
-    return $results;
-    /* foreach($results as $post) {
-        return $post->content;
-    } */
+     $posts  = Post::all();
+     
+     foreach($posts as $post) {
+        return $post->title;
+     }
 });
 
+Route::get('/find', function() {
+    $post = Post::find(2);
+    return $post->title;
+});
+
+Route::get('/findwhere', function(){
+    $posts = Post::where('id', 2)->orderBy('id', 'desc')->take(1)->get();
+    return $posts;
+});
+
+Route::get('/findmore', function() {
+    $posts = Post::findOrFail(4);
+    return $posts;
+});
+
+// INSERT
+Route::get('/basicinsert', function() {
+    $post = new Post;
+
+    $post->title = 'New Eloquent title insert';
+    $post->content = 'New Eloquent is really cool!';
+    $post->save();
+});
+
+Route::get('/findandupdate', function() {
+    $post = Post::find(2);
+
+    $post->title = 'New Eloquent title insert #2';
+    $post->content = 'New Eloquent is really cool! #2';
+    $post->save();
+});
+
+Route::get('/create', function() {
+    Post::create([
+        'title' => 'the create method',
+        'content' => 'I\'m learning a lot',
+    ]);
+});
+
+// UPDATE
 Route::get('/update', function() {
-    $updated = DB::update('UPDATE posts SET title = "Updated title" WHERE id = ?', [1]);
-
-    return $updated;
+    Post::where('id', 2)->where('is_admin', 0)->update(['title' => 'NEW PHP TITLE', 'content' => 'NEW CONTENT!!!']);
 });
 
+// DELETE
 Route::get('/delete', function() {
-    $deleted = DB::delete('DELETE FROM posts WHERE id = ?', [1]);
-    return $deleted;
+    $post = Post::find(1);
+    $post->delete();
 });
+
+Route::get('/destroy', function() {
+    Post::destroy(3);
+    
+    // deleting multiple
+    // Post::destroy([4,5]);
+
+    // another delete option
+    // Post::where('is_admin', 0)->delete();
+});
+
+// SOFT DELETE
+Route::get('/softdelete', function() {
+    Post::find(6)->delete();
+});
+
+Route::get('/readsoftdelete', function() {
+    $posts = Post::withTrashed()->where('is_admin', 0)->get();
+    // $posts = Post::onlyTrashed()->where('is_admin', 0)->get();
+
+    return $posts;
+});
+
+Route::get('/restore', function() {
+    $posts = Post::withTrashed()->where('is_admin', 0)->restore();
+    
+    return $posts;
+});
+
+// THIS METHOD FORCE DELETE RECORD WHEN SOFTDELETE IS ENABLED
+Route::get('/forcedelete', function() {
+    $posts = Post::onlyTrashed()->where('is_admin', 0)->forceDelete();
+    
+    return $posts;
+});
+
